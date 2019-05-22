d401e2d53080048cc9833d74e70ff10baf3cb74f
Unknown <skrzynska.konstancja@gmail.com>
2019-05-22 21:00:06 +0200

    set windows

--- main.js
+++ main.js
@@ -2,11 +2,12 @@ const electron = require('electron');
 const url = require('url');
 const path = require('path');
 
-const {app, BrowserWindow} = electron;
+const {app, BrowserWindow, Menu} = electron;
 
-console.log('Your App Path: + ', app.getAppPath());
+// console.log('Your App Path: + ', app.getAppPath());
 
 let mainWindow;
+let addWindow;
 // listen for app to be ready 
 app.on('ready',function(){
     // create new window
@@ -16,10 +17,66 @@ app.on('ready',function(){
         }
     });
     // load html 
-    console.log('path: ', __dirname)
+    // console.log('path: ', __dirname)
+    // file://dirname/mainWindow.html
     mainWindow.loadURL(url.format({
         pathname: path.join(__dirname, 'mainWindow.html'),
         protocol: 'file:',
         slashes: true
     }));
+    //  quit app when closed
+    mainWindow.on('closed', function(){
+        app.quit();
+    });
+
+    //  build menu from template
+    const mainMenu = Menu.buildFromTemplate(mainMenuTemplate);
+    // insert menu
+    Menu.setApplicationMenu(mainMenu);
 });
+
+// handle create add window
+function createAddWindow(){
+    //  create new window
+    addWindow = new BrowserWindow({
+        width: 300,
+        height: 200,
+        title: 'Add Shopping List Item',
+        webPreferences: {
+            nodeIntegration: true
+        }
+    });
+    addWindow.loadURL(url.format({
+        pathname: path.join(__dirname, 'addWindow.html'),
+        protocol: 'file:',
+        slashes: true
+    }));
+    //  garbage collection handle
+    addWindow.on('close', function(){
+        addWindow = null;
+    });
+}
+// create menu tempalte
+const mainMenuTemplate=[
+    {
+        label:'File',
+        submenu:[
+            {
+                label:'Add Item',
+                click(){
+                    createAddWindow()
+                }
+            },
+            {
+                label: 'Clear Items'
+            },
+            {
+                label:'Quit',
+                accelerator:process.platform=='darwin' ? 'Command+Q' : 'Ctrl+Q',
+                click(){
+                    app.quit();
+                }
+            }
+        ]
+    }
+]
